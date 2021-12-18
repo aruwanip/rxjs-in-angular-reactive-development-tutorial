2a4db740d6d4b70f4dd7583aa5cba1980b733d17
Pinkie Aruwani <aruwanip@gmail.com>
2021-12-18 14:27:40 -0500

    Using declarative pattern for data retrieval

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,33 +1,28 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { EMPTY, Observable } from 'rxjs';
-
-import { Product } from './product';
-import { ProductService } from './product.service';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { EMPTY } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+
+import { ProductService } from './product.service';
 
 @Component({
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ProductListComponent implements OnInit {
+export class ProductListComponent {
   pageTitle = 'Product List';
   errorMessage = '';
   categories;
 
-  products$: Observable<Product[]>;
+  products$ = this.productService.products$
+    .pipe(
+      catchError(err => {
+        this.errorMessage = err;
+        return EMPTY;
+      })
+    );
 
   constructor(private productService: ProductService) { }
-
-  ngOnInit(): void {
-    this.products$ = this.productService.getProducts()
-      .pipe(
-        catchError(err => {
-          this.errorMessage = err;
-          return EMPTY;
-        })
-      );
-  }
 
   onAdd(): void {
     console.log('Not yet implemented');
