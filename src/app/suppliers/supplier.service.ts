c9711751425d9f017e971183840ce8456bc7ad6d
Pinkie Aruwani <aruwanip@gmail.com>
2021-12-19 17:54:33 -0500

    Higher-order mapping operators: mergeMap

--- src/app/suppliers/supplier.service.ts
+++ src/app/suppliers/supplier.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { throwError, Observable, of } from 'rxjs';
-import { concatMap, map, tap } from 'rxjs/operators';
+import { concatMap, map, mergeMap, tap } from 'rxjs/operators';
 
 import { Supplier } from './supplier';
 
@@ -22,14 +22,19 @@ export class SupplierService {
       concatMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
     );
 
+  suppliersWithMergeMap$ = of(1, 5, 8)
+    .pipe(
+      tap(id => console.log('mergeMap source Observable', id)),
+      mergeMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+    );
+
   constructor(private http: HttpClient) {
     // this.suppliersWithMap$
     //   .subscribe(o => o.subscribe(
     //     item => console.log('map result', item)
     //   ));
-    this.suppliersWithConcatMap$.subscribe(
-      item => console.log('concatMap result', item)
-    );
+    this.suppliersWithConcatMap$.subscribe(item => console.log('concatMap result', item));
+    this.suppliersWithMergeMap$.subscribe(item => console.log('mergeMap result', item));
   }
 
   private handleError(err: any): Observable<never> {
