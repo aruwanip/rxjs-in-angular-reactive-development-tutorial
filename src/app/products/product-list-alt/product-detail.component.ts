978613f951ed18876f5bdf4285e212e710ab3535
Pinkie Aruwani <aruwanip@gmail.com>
2021-12-19 20:13:01 -0500

    Combining streams to use only one async pipe for view model data

--- src/app/products/product-list-alt/product-detail.component.ts
+++ src/app/products/product-list-alt/product-detail.component.ts
@@ -1,6 +1,6 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { EMPTY, Subject } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { combineLatest, EMPTY, Subject } from 'rxjs';
+import { catchError, filter, map } from 'rxjs/operators';
 
 import { Product } from '../product';
 import { ProductService } from '../product.service';
@@ -34,6 +34,16 @@ export class ProductDetailComponent {
         this.errorMessageSubject.next(err);
         return EMPTY
       })
+    );
+
+  vm$ = combineLatest([
+    this.product$,
+    this.productSuppliers$,
+    this.pageTitle$
+  ])
+    .pipe(
+      filter(([product]) => Boolean(product)),
+      map(([product, productSuppliers, pageTitle]) => ({ product, productSuppliers, pageTitle}))
     );
 
   constructor(private productService: ProductService) { }
